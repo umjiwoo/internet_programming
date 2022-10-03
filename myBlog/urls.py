c147174eaff68949dcc261d8d88a2866b2de915d
jwUm <87863185+umjiwoo@users.noreply.github.com>
2022-10-03 15:05:05 +0900

    CBV로 대문/자기소개 페이지 만들기

--- myBlog/urls.py
+++ myBlog/urls.py
@@ -3,6 +3,11 @@ from django.urls import path
 from myBlog import views
 
 urlpatterns = [
-    path('', views.index),
-    path('<int:pk>/', views.single_post_page),
+    # FunctionBasedView
+    # path('', views.index),
+    # path('<int:pk>/', views.single_post_page),
+
+    # ClssBasedView
+    path('', views.PostList.as_view()),
+    path('<int:pk>/', views.PostDetail.as_view()),
 ]
--- myBlog/views.py
+++ myBlog/views.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render
-
+from django.views.generic import ListView, DetailView
 from myBlog.models import Post
+# def index(request):
+#     posts = Post.objects.all().order_by('-pk')
+#     return render(request, 'blog/index.html', {'posts': posts})
+#
+#
+# def single_post_page(request, pk):
+#     post = Post.objects.get(pk=pk)
+#     return render(request, 'blog/single_post_page.html', {'post': post})
 
 
-def index(request):
-    posts = Post.objects.all().order_by('-pk')
-    return render(request, 'blog/index.html', {'posts': posts})
+# class view 이용하는 경우 템플릿 이름 자동 설정됨 ; 모델명_list.html, 모델명_detail.html
+# templates 하위 디렉토리 이름을 앱 이름과 동일하게 해줘야 파일 찾기 가능
+class PostList(ListView):
+    model = Post
+    ordering = '-pk'
 
 
-def single_post_page(request, pk):
-    post = Post.objects.get(pk=pk)
-    return render(request, 'blog/single_post_page.html', {'post': post})
+class PostDetail(DetailView):
+    model = Post
