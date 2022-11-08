0dce5b03656902636b40342ede50bebefc2e5e53
jwUm <87863185+umjiwoo@users.noreply.github.com>
2022-11-08 16:00:02 +0900

    포스트 작성 페이지 만들기

--- myBlog/urls.py
+++ myBlog/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     # ClssBasedView
     path('', views.PostList.as_view()),
     path('<int:pk>/', views.PostDetail.as_view()),
+    path('create_post/', views.PostCreate.as_view()),
     path('category/<str:slug>/', views.category_page),
     path('tag/<str:slug>/', views.tag_page),
 ]
--- myBlog/views.py
+++ myBlog/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.shortcuts import render, redirect
+from django.views.generic import ListView, DetailView, CreateView
 from myBlog.models import Post, Category, Tag
+
+
 # def index(request):
 #     posts = Post.objects.all().order_by('-pk')
 #     return render(request, 'blog/index.html', {'posts': posts})
@@ -16,8 +19,10 @@ from myBlog.models import Post, Category, Tag
 class PostList(ListView):
     model = Post
     ordering = '-pk'
+
     # 매개변수 => post_list
 
+    # get_context_data() => 페이지에 원하는 내용 띄우기 위해 생성한 함수
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super(PostList, self).get_context_data()
         context['categories'] = Category.objects.all()
@@ -27,10 +32,38 @@ class PostList(ListView):
 
 class PostDetail(DetailView):
     model = Post
+
     # 매개변수 => post
 
     def get_context_data(self, **kwargs):
         context = super(PostDetail, self).get_context_data()  # 템플릿으로 전달할 내용을 담음
+        context['categories'] = Category.objects.all()
+        context['no_category_post_count'] = Post.objects.filter(category=None).count
+        return context
+
+
+class PostCreate(CreateView, LoginRequiredMixin, UserPassesTestMixin):
+    model = Post
+    fields = ['title', 'hook_text', 'content', 'head_image', 'file_upload', 'category']
+
+    # CreateView 호출 뷰 ->  모델명_form.html
+    # 뷰에 form 이름으로 전달됨
+
+    # ModelFormMixin 클래스 상속 함수 -> 입력받은 내용이 모델에 적합한지 확인
+
+    def test_func(self):
+        return self.reqeust.user.is_superuser or self.request.user.is_staff
+
+    def form_valid(self, form):
+        current_user = self.request.user
+        if current_user.is_authenticated and current_user.is_superuser or current_user.is_staff:  # 요청 보낸 유저가 인가된 유저라면
+            form.instance.author = current_user
+            return super(PostCreate, self).form_valid(form)
+        else:
+            return redirect('/blog/')
+
+    def get_context_data(self, **kwargs):
+        context = super(PostCreate, self).get_context_data()  # 템플릿으로 전달할 내용을 담음
         context['categories'] = Category.objects.all()
         context['no_category_post_count'] = Post.objects.filter(category=None).count
         return context
